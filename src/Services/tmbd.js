cb5e8dddb7c864cdeb8e2e1c3ca836449c870b42
Line <anna.line2507@gmail.com>
2025-06-04 15:30:42 +0200

    added comments to services

--- src/Services/tmbd.js
+++ src/Services/tmbd.js
@@ -2,12 +2,15 @@
 const API_KEY = '4e461f739d78e77d2d7f16407e3db2c7'
 const BASE_URL = 'https://api.themoviedb.org/3'
 
+// Fetches a list of popular movies from TMDB.
+// Returns an array of movie objects.
 export const fetchPopularMovies = async () => {
   const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
   const data = await res.json()
   return data.results
 }
 
+//  Fetches movies filtered by a specific genre ID from TMDB.
 export const fetchMoviesByGenre = async (genreId) => {
   const res = await fetch(
     `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&language=en-US`
@@ -16,6 +19,8 @@ export const fetchMoviesByGenre = async (genreId) => {
   return data.results
 }
 
+// Fetches all available movie genres from TMDB.
+// Returns an array of genre objects (id and name).
 export const fetchGenres = async () => {
   const res = await fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`)
   const data = await res.json()
--- src/Services/watchlistService.js
+++ src/Services/watchlistService.js
@@ -1,17 +1,21 @@
+// Imports
 import { db } from '../firebaseConfig'
 import { collection, doc, getDocs, setDoc, deleteDoc } from 'firebase/firestore'
 
+// Fetches the current user's watchlist from Firestore.
 export const getWatchlist = async (uid) => {
   const colRef = collection(db, 'users', uid, 'watchlist')
   const snapshot = await getDocs(colRef)
   return snapshot.docs.map(doc => doc.data())
 }
 
+// Adds a movie to the user's watchlist in Firestore.
 export const addToWatchlist = async (uid, movie) => {
   const docRef = doc(db, 'users', uid, 'watchlist', movie.id.toString())
   await setDoc(docRef, movie)
 }
 
+// Removes a movie from the user's watchlist in Firestore.
 export const removeFromWatchlist = async (uid, movieId) => {
   const docRef = doc(db, 'users', uid, 'watchlist', movieId.toString())
   await deleteDoc(docRef)
